b9027157f16722a9dda199e8e7aa8bc104b319d7
Devin Cooley <dcooley1350@gmail.com>
2019-06-03 19:47:17 -0700

    complete css and fix return val bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
-    $(".person1").append("blah blah");
-    $(".adjective1").append("blah blah");
-    $(".verb1").append("blah blah");
-    $(".food1").append("blah blah");
-    $(".adjective2").append("blah blah");
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var adjective1Input = $("input#adjective1").val();
+    var verb1Input = $("input#verb1").val();
+    var food1Input = $("input#food1").val();
+    var adjective2Input = $("input#adjective2").val();
+
+    $(".person1").text(person1Input);
+    $(".adjective1").text(adjective1Input);
+    $(".verb1").text(verb1Input);
+    $(".food1").text(food1Input);
+    $(".adjective2").text(adjective2Input);
 
     $("#story").show();
 
     event.preventDefault();
-  })
-})
+  });
+});
